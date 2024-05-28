f19f4d9089a861d4db7c7c2db8224de2ef1aa513
Dogolaa <lucasdogo01@gmail.com>
2024-05-28 20:14:41 -0300

    Calculando Media

---
+++ CalculandoMedia.sql
@@ -0,0 +1,9 @@
+SET SERVEROUTPUT ON
+DECLARE
+    vNotaP1 NUMBER(11,2) := 5;
+    vNotaP2 NUMBER(11,2) := 10;
+    vMedia NUMBER(11,2);
+BEGIN
+    vMedia := ((vNotaP1 + vNotaP2) / 2);
+    DBMS_OUTPUT.PUT_LINE('A MEDIA DO ALUNO EH: ' || vMedia);
+END;
