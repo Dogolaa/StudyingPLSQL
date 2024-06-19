16eec92d9d42901730ef622a2f6b495e6d7a2347
Dogolaa <lucasdogo01@gmail.com>
2024-06-19 15:40:46 -0300

    Outra opcao de CASE

--- CASE.sql
+++ CASE.sql
@@ -28,3 +28,34 @@ DBMS_OUTPUT.PUT_LINE('Percentual: ' || vpercentual);
     WHERE department_id = vdepartment_id;
     --COMMIT
 END;
+
+--Outra forma
+
+SET SERVEROUTPUT ON
+ACCEPT  pdepartment_id PROMPT 'Digite o ID do departamento'
+DECLARE
+   vdepartment_id  employees.department_id%type := &pdepartment_id;
+   vpercentual     NUMBER(3);
+BEGIN
+    CASE 
+    WHEN vdepartment_id = 80
+    THEN
+        vpercentual := 10;              --Sales
+    WHEN vdepartment_id = 20
+    THEN
+            vpercentual := 15;          --Marketing
+    WHEN vdepartment_id = 60
+    THEN
+    vpercentual := 20;          --IT
+    ELSE
+    vpercentual := 5;
+    END CASE;
+    
+DBMS_OUTPUT.PUT_LINE('Id do Departamento: ' || vDepartment_id);
+DBMS_OUTPUT.PUT_LINE('Percentual: ' || vpercentual);
+
+    UPDATE employees
+    SET salary = salary * ( 1 + vpercentual / 100)
+    WHERE department_id = vdepartment_id;
+    --COMMIT
+END;
